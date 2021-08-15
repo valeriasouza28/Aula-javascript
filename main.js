b0109dd55a77ad44e3fd1fdf4fc1ed2aef6b342a
valeriasouza28 <82419677+valeriasouza28@users.noreply.github.com>
2021-08-14 21:08:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var nome = "Valéria Souza";
+var n1 = 23;
+var n2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert(`${nome} tem ${idade} anos`);
+console.log(nome);
+console.log(n1 * n2);
+/* Usando o replace para alterar a palavra Japão para Brasil dentro console.log*/
+/*console.log(frase.replace("Japão" , "Brasil")); */
+//alert(frase.replace("Japão" , "Brasil"))
+console.log(frase.toLowerCase())
